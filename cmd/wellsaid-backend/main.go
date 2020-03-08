681125a60ff9c99b40332be34cf80a7b9829c4bf
Dan Stutzman <dan@danstutzman.com>
2020-03-08 10:06:41 -0600

    Log 404s

--- cmd/wellsaid-backend/main.go
+++ cmd/wellsaid-backend/main.go
@@ -2,12 +2,49 @@ package main
 
 import (
 	"bitbucket.org/danstutzman/wellsaid-backend/internal/db"
+	"database/sql"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
 	"time"
 )
+
+func handleIndex(w http.ResponseWriter, r *http.Request, dbConn *sql.DB) {
+	receivedAt := time.Now().UTC()
+
+	output := "This is a catch-all route\n"
+	w.Write([]byte(output))
+
+	db.InsertIntoRequests(dbConn, db.RequestsRow{
+		ReceivedAt: receivedAt,
+		RemoteAddr: r.RemoteAddr,
+		UserAgent:  r.UserAgent(),
+		Referer:    r.Referer(),
+		Method:     r.Method,
+		Path:       r.URL.RequestURI(),
+		StatusCode: 200,
+		Size:       len(output),
+	})
+}
+
+func handleNotFound(w http.ResponseWriter, r *http.Request, dbConn *sql.DB) {
+	receivedAt := time.Now().UTC()
+
+	output := "Not found\n"
+	w.Write([]byte(output))
+
+	db.InsertIntoRequests(dbConn, db.RequestsRow{
+		ReceivedAt: receivedAt,
+		RemoteAddr: r.RemoteAddr,
+		UserAgent:  r.UserAgent(),
+		Referer:    r.Referer(),
+		Method:     r.Method,
+		Path:       r.URL.RequestURI(),
+		StatusCode: 404,
+		Size:       len(output),
+	})
+}
 
 func main() {
 	httpPort := os.Getenv("HTTP_PORT")
@@ -22,22 +59,12 @@ func main() {
 	dbConn := db.InitDb(dbFile)
 
 	router := mux.NewRouter()
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		receivedAt := time.Now().UTC()
-
-		output := "This is a catch-all route\n"
-		w.Write([]byte(output))
-
-		db.InsertIntoRequests(dbConn, db.RequestsRow{
-			ReceivedAt: receivedAt,
-			RemoteAddr: r.RemoteAddr,
-			UserAgent:  r.UserAgent(),
-			Referer:    r.Referer(),
-			Method:     r.Method,
-			Path:       r.URL.RequestURI(),
-			StatusCode: 200,
-			Size:       len(output),
+	router.NotFoundHandler = http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			handleNotFound(w, r, dbConn)
 		})
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handleIndex(w, r, dbConn)
 	})
 
 	log.Printf("Listening on :" + httpPort + "...")
